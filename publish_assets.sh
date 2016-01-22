ab0fa64d54eaff82e2002f9b4726e4cde23938d3
Adyg <ady.ghiuta@gmail.com>
2016-01-23 00:36:24 +0200

    no need to cd in the project dir anymore

--- publish_assets.sh
+++ publish_assets.sh
@@ -1,4 +1,3 @@
-cd {{project_name}} && \
 env SECRET_KEY=`heroku config:get SECRET_KEY --app={{project_name}}` \
     AWS_SECRET_ACCESS_KEY=`heroku config:get AWS_SECRET_ACCESS_KEY --app={{project_name}}` \
     AWS_ACCESS_KEY_ID=`heroku config:get AWS_ACCESS_KEY_ID --app={{project_name}}` \
