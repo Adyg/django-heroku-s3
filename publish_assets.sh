076f9f5db25aa918183e0f27a90a6e0b47873791
Adrian Ghiuta <ady.ghiuta@gmail.com>
2017-06-02 01:07:17 +0300

    path to python is not needed

--- publish_assets.sh
+++ publish_assets.sh
@@ -3,5 +3,5 @@ env SECRET_KEY=`heroku config:get SECRET_KEY --app={{project_name}}` \
     AWS_ACCESS_KEY_ID=`heroku config:get AWS_ACCESS_KEY_ID --app={{project_name}}` \
     AWS_STORAGE_BUCKET_NAME=`heroku config:get AWS_STORAGE_BUCKET_NAME --app={{project_name}}` \
     STATIC_ROOT='/vagrant/assets' \
-    /usr/bin/python manage.py collectstatic --settings={{project_name}}.settings.heroku
+    python manage.py collectstatic --settings={{project_name}}.settings.heroku
 cd ..
